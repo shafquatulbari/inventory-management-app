3f5e6ce4250574638092f8052bb0a6a3f30587e1
SHAFQUAT UL BARI <77532312+shafquatulbari@users.noreply.github.com>
2023-06-20 11:00:15 -0700

    more comments

--- inventory-management/src/App.js
+++ inventory-management/src/App.js
@@ -7,8 +7,9 @@ import '../src/App.css';
 function Detail({ item }) {
   // Displays item details
   return (
+    // JSX for item's details
       <div>
-          <h3>{item.name}</h3>
+          <h3>{item.name}</h3> 
           <p>{item.description}</p>
           <p>${item.price}</p>
           <img className="item-image" src={item.image} alt={item.name} />
@@ -20,86 +21,93 @@ function Detail({ item }) {
 useDispatch is another hook from react-redux that gives you access to the dispatch function from the Redux store. 
 You're also defining some state variables using the useState hook from React.*/
 
-//App component
+// App is the main functional component of the application
 function App() {
-  const items = useSelector((state) => state);
-  const dispatch = useDispatch();
+  const items = useSelector((state) => state); // Use the useSelector hook to access items from the Redux store
+  const dispatch = useDispatch(); // Use the useDispatch hook to get the dispatch function to dispatch actions
   
+  // Define local state variables for form inputs
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [image, setImage] = useState("");
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [editing, setEditing] = useState(false);
-  const [editedName, setEditedName] = useState("");
+  const [selectedItem, setSelectedItem] = useState(null); // State to keep track of selected item for Detail
+  const [editing, setEditing] = useState(false); // State to switch between editing and adding new item
+  const [editedName, setEditedName] = useState(""); // State to keep the original name of the item being edited
 
+  // useEffect hook to fetch data from server when component mounts
   useEffect(() => {
-    fetch('http://localhost:3000/items')
-      .then(response => response.json())
-      .then(data => {
-        dispatch(clearItems());
-        data.forEach(item => dispatch(addItem(item)));
+    fetch('http://localhost:3000/items') // Fetch items from server
+      .then(response => response.json()) // Parse JSON response
+      .then(data => { // Process data
+        dispatch(clearItems()); // Clear existing items from Redux store
+        data.forEach(item => dispatch(addItem(item))); // Add each item from server to Redux store
       });
-  }, [dispatch]);
+  }, [dispatch]); // Only rerun this effect if dispatch function changes (which should never happen)
 
+  // Function to handle form submission for adding a new item
   const handleSubmit = (e) => {
-      e.preventDefault();
-      const newItem = { name, description, price, image };
-      fetch('http://localhost:3000/items', {
+      e.preventDefault(); // Prevent form from refreshing page
+      const newItem = { name, description, price, image }; // Construct new item object from form inputs
+      fetch('http://localhost:3000/items', { // Send POST request to server
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newItem)
+        headers: { 'Content-Type': 'application/json' }, //took help from CHATGPT to implement the fetch method calls
+        body: JSON.stringify(newItem) // Stringify new item object to JSON for request body
       }).then(() => {
-        dispatch(addItem(newItem));
-        setName("");
+        dispatch(addItem(newItem)); // Add new item to Redux store
+        setName(""); // Clear form inputs
         setDescription("");
         setPrice("");
         setImage("");
       });
   };
 
-  const handleClear = () => {
-      setName("");
+   // Function to handle form reset
+  const handleClear = () => { 
+      setName(""); // Clear form inputs
       setDescription("");
       setPrice("");
       setImage("");
   };
 
+  // Function to handle deleting an item
   const handleDelete = (item) => {
-      fetch(`http://localhost:3000/items/${item.name}`, {
+      fetch(`http://localhost:3000/items/${item.name}`, { // Send DELETE request to server for specified item
         method: 'DELETE',
       }).then(() => {
-        dispatch(deleteItem(item));
-        if (selectedItem === item) {
-          setSelectedItem(null);
+        dispatch(deleteItem(item)); // Remove item from Redux store
+        if (selectedItem === item) { // If deleted item is currently selected
+          setSelectedItem(null); // Deselect item
         }
       });
   };
 
+  // Function to handle initiating item editing
   const handleEdit = (item) => {
-      setName(item.name);
+      setName(item.name); // Set form inputs to current values of item
       setDescription(item.description);
       setPrice(item.price);
       setImage(item.image);
-      setEditedName(item.name);
-      setEditing(true);
+      setEditedName(item.name); // Keep track of original item name for server request
+      setEditing(true); // Switch form to editing mode
   };
 
+  // Function to handle form submission for updating an item
   const handleUpdate = (e) => {
-      e.preventDefault();
-      const updatedItem = { name, description, price, image };
-      fetch(`http://localhost:3000/items/${editedName}`, {
+      e.preventDefault(); //// Prevent form from refreshing page
+      const updatedItem = { name, description, price, image }; // Construct updated item object from form inputs
+      fetch(`http://localhost:3000/items/${editedName}`, { // Send PUT request to server for specified item
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(updatedItem)
-      }).then(response => response.json())
+        body: JSON.stringify(updatedItem) // Stringify updated item object to JSON for request body
+      }).then(response => response.json()) // Parse JSON response
       .then(data => {
-        dispatch(editItem(data));
-        setName("");
+        dispatch(editItem(data)); // Update item in Redux store
+        setName(""); // Clear form inputs
         setDescription("");
         setPrice("");
         setImage("");
-        setEditing(false);
+        setEditing(false); // Switch form back to adding mode
       });
   };
 
@@ -111,26 +119,27 @@ function App() {
     return (
       <div className="App">
             <h1>My Inventory</h1>
-            <form onSubmit={editing ? handleUpdate : handleSubmit}>
-                <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Item Name" />
-                <input type="text" value={description} onChange={e => setDescription(e.target.value)} placeholder="Description" />
-                <input type="number" value={price} onChange={e => setPrice(e.target.value)} placeholder="$$$" />
-                <input type="text" value={image} onChange={e => setImage(e.target.value)} placeholder="Image URL" />
-                <button type="submit">{editing ? 'Update Item' : 'Add Item'}</button>
-                <button type="button" onClick={handleClear}>Clear</button>
+            <form onSubmit={editing ? handleUpdate : handleSubmit}> {/*Form switches between adding and updating based on editing state*/}
+                <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Item Name" /> {/*Input for item name*/}
+                <input type="text" value={description} onChange={e => setDescription(e.target.value)} placeholder="Description" /> {/*Input for Description*/}
+                <input type="number" value={price} onChange={e => setPrice(e.target.value)} placeholder="$$$" /> {/*Input for price*/}
+                <input type="text" value={image} onChange={e => setImage(e.target.value)} placeholder="Image URL" /> {/*Input for image link*/}
+                <button type="submit">{editing ? 'Update Item' : 'Add Item'}</button> {/*Submit button text changes based on editing state*/}
+                <button type="button" onClick={handleClear}>Clear</button> {/*Clear form inputs on click*/}
             </form>
-            {items.map(item => (
-                <div key={item.name} onClick={() => setSelectedItem(item)}>
+            {/*Map over items in Redux store and create a div for each*/}
+            {items.map(item => ( 
+                <div key={item.name} onClick={() => setSelectedItem(item)}> {/*Clicking on div selects item for Detail*/}
                     <h3>{item.name}</h3>
                     <img className="item-image" src={item.image} alt={item.name} />
-                    <button onClick={(e) => {e.stopPropagation(); handleDelete(item)}}>Delete</button>
-                    <button onClick={(e) => {e.stopPropagation(); handleEdit(item)}}>Edit</button>
+                    <button onClick={(e) => {e.stopPropagation(); handleDelete(item)}}>Delete</button> {/*Button to delete item*/}
+                    <button onClick={(e) => {e.stopPropagation(); handleEdit(item)}}>Edit</button> {/*Button to edit item*/}
                 </div>
             ))}
-            {selectedItem && <Detail item={selectedItem} />}
+            {selectedItem && <Detail item={selectedItem} />} {/*If an item is selected, display Detail component for item*/}
         </div>
   );
 }
 
-export default App;
-//Used Chatgpt for commenting code
+export default App; // Export App component for use in other modules
+//Used Chatgpt for commenting code and understanding the fetch method calls and implementation
--- inventory-management/src/redux/items.js
+++ inventory-management/src/redux/items.js
@@ -1,11 +1,13 @@
 // Action types
+// Define a string constant for each type of action our Redux store will handle
 const ADD_ITEM = 'ADD_ITEM';
 const DELETE_ITEM = 'DELETE_ITEM';
 const CLEAR_ITEMS = 'CLEAR_ITEMS';
 const EDIT_ITEM = 'EDIT_ITEM';
 
 //ADD_ITEM and DELETE_ITEM are constants representing action types. The addItem and deleteItem functions are action creators.
-// Action creators
+// Action creators return an action object, which has a 'type' and optionally a 'payload'
+// Action creator for adding an item
 export const addItem = (item) => ({
     //type field (which represents the action type)
     type: ADD_ITEM,
@@ -33,25 +35,33 @@ const initialState = [];
 // Reducer
 //The itemsReducer is a function that takes the current state and an action as arguments and returns the new state
 const itemsReducer = (state = initialState, action) => {
+    // Depending on the type of the action, we will have different state transformations
     switch (action.type) {
         case ADD_ITEM:
-            // If action is ADD_ITEM, add the payload to the state
+            // If action is ADD_ITEM, add the payload to the state, create a new array with all the old items plus the new item
             return [...state, action.payload];
         case DELETE_ITEM:
             //The reducer uses the ... (spread) operator to return a new state array that includes the new item or exclude the deleted item, rather than modifying the existing state array. This is an example of reducer immutability.
             // If action is DELETE_ITEM, remove the payload from the state
+            // If action is DELETE_ITEM, create a new array with all the items except the one to delete
             return state.filter(item => item !== action.payload);
         case CLEAR_ITEMS:
+            // If action is CLEAR_ITEMS, return an empty array
             return [];
         case EDIT_ITEM:
+            // If action is EDIT_ITEM, create a new array where the edited item is replaced
+            //map function to create a new array that will replace the current state. 
+            //The map function goes through each item in the array and applies a function to it, returning a new array with the transformed items.
             return state.map(item => 
                 item.name === action.payload.name ? action.payload : item
             );
+           //If the name of the current item being processed is the same as the name of the item we're looking to update, 
+           //use the updated item (action.payload) in the new array. If not, just use the current item as is".
         default:
-            // If action is none of the above, return the current state
+            // If action type is not recognized(none of the above), return the current state without modification
             return state;
     }
 }
 
-export default itemsReducer;
+export default itemsReducer; // Export our reducer so it can be combined with other reducers in the store
 //Used Chatgpt for commenting code
